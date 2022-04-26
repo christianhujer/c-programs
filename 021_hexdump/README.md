e60ec03bae1fde813cdff372c7c2ac50fd75f0a2
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-04-26 12:43:54 +0530

    Add HTML hexdump version.

---
+++ 021_hexdump/README.md
@@ -0,0 +1,3 @@
+# 021 hexdump
+
+This program is a hex dump program that generates (X)HTML output.
--- 021_hexdump/hexdump.c
+++ 021_hexdump/hexdump.c
@@ -13,7 +13,7 @@ bool isEndOfLine(size_t bytesRead) {
     return bytesRead % LINE_LENGTH == LINE_LENGTH - 1;
 }
 
-char line[] = "00000000  0U UU UU UU UU UU UU UU UU UU UU UU UU UU UU UU";
+char line[] = "<tr><td><code>00000000</code></td><td><code>0U UU UU UU UU UU UU UU UU UU UU UU UU UU UU UU</code></td></tr>";
 
 char digits[] = "0123456789abcdef";
 
@@ -26,34 +26,68 @@ int main(void) {
     size_t bytesRead = 0;
     char *ptr;
 
+    puts("<?xml version=\"1.0\"?>");
+    puts("<!DOCTYPE html>");
+    puts("<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">");
+    puts("<head>");
+    puts("<title>Hex Dump</title>");
+    puts("</head>");
+    puts("<body>");
+    puts("<h1>Hex Dump</h1>");
+    puts("<table border=\"border\">");
+    puts("<thead>");
+    puts("<tr><th scope=\"col\">Offset</th><th scope=\"col\">Data</th></tr>");
+    puts("</thead>");
+    puts("<tbody>");
+
     for (int c; (c = getchar()) != EOF; bytesRead++) {
         if (isStartOfLine(bytesRead)) {
             ptr = line;
+            *ptr++ = '<'; *ptr++ = 't'; *ptr++ = 'r'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
             for (int shift = 28; shift >= 0; shift -= 4) {
                 *ptr++ = toASCII((bytesRead >> shift) & 0xF);
             }
+            *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+        } else {
             *ptr++ = ' ';
         }
-        *ptr++ = ' ';
         *ptr++ = toASCII((c & 0xF0) >> 4);
         *ptr++ = toASCII((c & 0x0F) >> 0);
 
-        if (isEndOfLine(bytesRead))
+        if (isEndOfLine(bytesRead)) {
+            *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+            *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'r'; *ptr++ = '>';
+            *ptr++ = '\0';
             puts(line);
+        }
     }
     if (bytesRead % 16 != 0) {
+        *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+        *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+        *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'r'; *ptr++ = '>';
         *ptr++ = '\0';
         puts(line);
     }
     ptr = line;
-    *ptr++ = toASCII((bytesRead >> 28) & 0xF);
-    *ptr++ = toASCII((bytesRead >> 24) & 0xF);
-    *ptr++ = toASCII((bytesRead >> 20) & 0xF);
-    *ptr++ = toASCII((bytesRead >> 16) & 0xF);
-    *ptr++ = toASCII((bytesRead >> 12) & 0xF);
-    *ptr++ = toASCII((bytesRead >>  8) & 0xF);
-    *ptr++ = toASCII((bytesRead >>  4) & 0xF);
-    *ptr++ = toASCII((bytesRead >>  0) & 0xF);
+    *ptr++ = '<'; *ptr++ = 't'; *ptr++ = 'r'; *ptr++ = '>';
+    *ptr++ = '<'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+    *ptr++ = '<'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+    for (int shift = 28; shift >= 0; shift -= 4) {
+        *ptr++ = toASCII((bytesRead >> shift) & 0xF);
+    }
+    *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 'c'; *ptr++ = 'o'; *ptr++ = 'd'; *ptr++ = 'e'; *ptr++ = '>';
+    *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'd'; *ptr++ = '>';
+    *ptr++ = '<'; *ptr++ = '/'; *ptr++ = 't'; *ptr++ = 'r'; *ptr++ = '>';
     *ptr++ = '\0';
     puts(line);
+    puts("</tbody>");
+    puts("</table>");
+    puts("</body>");
+    puts("</html>");
 }
